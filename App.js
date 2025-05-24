21b8d84f9b4267cddc7c568ed06105344e1ae3be
S <S>
2025-05-24 09:12:17 +0800

    placeholder ui

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native Final</Text>
+      <Text style={styles.largeText}>Best Fit</Text>
+      <Text style={styles.box}>1.0.0</Text>
+      <Text>Start</Text>
+      <Text>Design</Text>
+      <Text>Help</Text>
+      <Text>Settings</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  smallText: {
+    fontSize: 12,
+  },
+  mediumText: {
+    fontSize: 18,
+  },
+  largeText: {
+    fontSize: 24,
+  },
+  box: {
+    paddingBottom: 100,
+    margin: 10,
+  }
 });
